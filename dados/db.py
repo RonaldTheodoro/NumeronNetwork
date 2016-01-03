16664b7de5e8eb55b1bcf674d6aa5cad01eaeee8
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-01-03 17:27:27 -0200

    correção do arquivo db.py

--- dados/db.py
+++ dados/db.py
@@ -9,16 +9,16 @@ class DB(object):
     """
 
     def __init__(self, nameDB):
-        if os.path.exists('lojas.db'):
+        try:
             self.conn = sqlite3.connect(nameDB)
             self.cursor = self.conn.cursor()
-        else:
+        except sqlite3.Error:
             print('Banco de dados corrompido ou não encontrado')
             return False
 
     # Recebe uma string e executa o comando no DB
-    def ExecuteDB(self, command):
-        self.cursor.execute(command)
+    def ExecuteDB(self, c):
+        self.cursor.execute(c)
 
     # Retorna o resultado de uma consulta
     def FetchOneDB(self):
@@ -45,15 +45,25 @@ class QueryDB(object):
 
     def __init__(self):
         self.DB = DB('lojas.db')
+        if self.DB == False:
+            input('Banco de dados não encontrado')
 
     # Recebe um codigo de loja efetua a consulta no DB
     def SearchCodLojaDB(self, codloja):
-        command = 'SELECT * FROM lojas WHERE codloja = "{}"'.format(codloja)
-        DB.ExecuteDB(command)
+        c = 'SELECT * FROM lojas WHERE codloja = "{}"'
+        self.DB.ExecuteDB(c.format(codloja))
 
-    # Recebe o tipo da busca e retorna o resultado
+    # Recebe o nome de um supervisor e busca suas lojas
+    def SearchSupDB(self, sup):
+        c = 'SELECT codloja FROM lojas WHERE supervisor LIKE "%{}%"'
+        self.DB.ExecuteDB(c.format(sup.upper()))
+
+    # Retorna uma lista de lojas
     def ShowLastQuery(self, typequery):
+        # Retorna uma tupla com os dados de uma loja
         if typequery == 'one':
-            print(DB.FetchOneDB())
+            return self.DB.FetchOneDB()
+
+        # Retorna uma lista de tuplas com os dados de varias lojas
         elif typequery == 'all':
-            print(DB.FetchAllDB())
+            return self.DB.FetchAllDB()
